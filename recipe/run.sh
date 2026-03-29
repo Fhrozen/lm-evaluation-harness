6749ce9e37bb9759757d6cbc48039c96f80fb8e9
Fhrozen <nelson.yalta@ieee.org>
2026-03-29 14:17:39 +0900

    fix: update run script to handle multiple model types and improve usage instructions

--- recipe/run.sh
+++ recipe/run.sh
@@ -5,16 +5,46 @@ set -euo pipefail
 # Activate the conda environment
 source ../.venv/bin/activate
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <pretrained_model_path>"
+if [ "$#" -gt 2 ]; then
+    echo "Usage: $0 <pretrained_model_path> <type_model>"
+    exit 1
+elif [ "$#" -eq 2 ]; then
+    pretrained=$1
+    type_model=$2
+elif [ "$#" -eq 1 ]; then
+    pretrained=$1
+    type_model="base"
+else
+    echo "Usage: $0 <pretrained_model_path> [type_model]"
     exit 1
 fi
 
-pretrained=$1
+# Original Base tasks: hellaswag,piqa,winogrande,arc_challenge,mmlu,truthfulqa_mc2,gsm8k
+# Tasks from SmolLM2
+base_tasks="hellaswag,arc_challenge,arc_easy,piqa,mmlu,commonsense_qa,triviaqa,winogrande,openbookqa,gsm8k"
+
+# Tasks from https://huggingface.co/blog/codelion/optimal-model-architecture
+base_tasks+=",truthfulqa_mc2"
+
+# OBQA: openbookqa
+# Tasks from MobileLLM-Pro paper
+base_tasks+=",boolq,social_iqa,nq_open,longbench,longbench_e"
+
+if [ "$type_model" = "base" ]; then
+    tasks=$base_tasks
+elif [ "$type_model" = "instruct" ]; then
+    echo "ERROR: Not implemented yet for instruct models. Please implement the tasks for instruct models in the script."
+    exit 1
+    tasks="${base_tasks},gsm8k,truthfulqa_mc2"
+else
+    echo "Invalid type_model: $type_model. Valid options are: base, instruct."
+    exit 1
+fi
 
 # Run the Python script
 accelerate launch -m lm_eval --model hf \
     --model_args pretrained=${pretrained},dtype="bfloat16",trust_remote_code=True \
-    --tasks hellaswag,piqa,winogrande,arc_challenge,mmlu,truthfulqa_mc2,gsm8k \
+    --tasks ${tasks} \
     --batch_size auto:8 \
     --output_path results \
+    --log_samples || exit 1
